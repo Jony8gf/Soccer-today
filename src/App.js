1e6eba62b2eb8639675ef16d88e0eef006e3209e
Jonathan <jgonzalezf09@educantabria.es>
2021-10-28 17:23:12 +0200

    Version 1.1
    Version 1.1

--- src/App.js
+++ src/App.js
@@ -1,39 +1,67 @@
 import { useState, useEffect } from "react";
 import Header from './Components/Header'
+import EquipoImage from './Components/EquipoImage'
+
+import DatePicker from "react-datepicker";
+
+// import required css from library
+import "react-datepicker/dist/react-datepicker.css";
+//https://codesource.io/complete-react-datepicker-tutorial-with-examples/
+
+let url = 'https://api.football-data.org/v2/matches'
+
+const tiempoTranscurrido = Date.now();
+let hoy = new Date(tiempoTranscurrido);
+let dateYear = hoy.toISOString()
+dateYear = dateYear.slice(0, 4);
+let dateMouth = hoy.toISOString()
+dateMouth = dateMouth.slice(5, 7);
+let dateDay = hoy.toISOString()
+dateDay = dateDay.slice(8, 10);
 
 const App = () => {
-
-  let url = 'https://api.football-data.org/v2/matches'
-
-  const tiempoTranscurrido = Date.now();
-  const hoy = new Date(tiempoTranscurrido);
-  var dateYear = hoy.toISOString()
-  dateYear = dateYear.slice(0, 4);
-  var dateMouth = hoy.toISOString()
-  dateMouth = dateMouth.slice(5, 7);
-  var dateDay = hoy.toISOString()
-  dateDay = dateDay.slice(8, 10);
-  var jsonCount = null
-  var jsonMatches = null
 
   url = 'https://api.football-data.org/v2/matches?dateFrom='+dateYear+'-'+dateMouth+'-'+dateDay+'&dateTo='+dateYear+'-'+dateMouth+'-'+dateDay
   //console.log(dateYear + '-' + dateMouth + '-' + dateDay )
 
+  const [matches, setMatches] = useState([])
+  useEffect(() => {
+    llamadaApi()   
+  }, [])
+
+  const [startDate, setStartDate] = useState(hoy)
+  
   const sumarDia = () =>{
+    hoy.setHours(+24)
+    console.log(hoy)
     dateDay++
     url = 'https://api.football-data.org/v2/matches?dateFrom='+dateYear+'-'+dateMouth+'-'+dateDay+'&dateTo='+dateYear+'-'+dateMouth+'-'+dateDay
-    llamadaApi()
     console.log(dateDay)
+    llamadaApi()
   }
   
-  const restarDia = () =>{
+  const restarDia = () =>{ 
+    hoy.setHours(-24)
+    console.log(hoy)
     dateDay--
     url = 'https://api.football-data.org/v2/matches?dateFrom='+dateYear+'-'+dateMouth+'-'+dateDay+'&dateTo='+dateYear+'-'+dateMouth+'-'+dateDay
-    llamadaApi()
     console.log(dateDay)
+    llamadaApi()
+  }
+
+  const cambiarFecha = () =>{
+    let dateChange = document.getElementById('datePicker').value
+    dateYear = dateChange.slice(0, 4);
+    dateMouth = dateChange.slice(5, 7);
+    dateDay = dateChange.slice(8, 10);
+    //console.log(dateChange)
+    console.log(dateYear + '-' + dateMouth + '-' + dateDay )
+    url = 'https://api.football-data.org/v2/matches?dateFrom='+dateYear+'-'+dateMouth+'-'+dateDay+'&dateTo='+dateYear+'-'+dateMouth+'-'+dateDay
+    llamadaApi()
   }
 
 const llamadaApi = async () => {
+
   const response = await fetch(url, {
     method: 'GET',
     headers: {
@@ -43,19 +71,9 @@ const llamadaApi = async () => {
 
   const json = await response.json()
   console.log(json)
-
-  //jsonMatches = json['matches']
-	// jsonCount = json['count']
-  //console.log(jsonMatches)
-  // console.log(jsonCount)
-
   setMatches(json['matches'])
-}
 
-  const [matches, setMatches] = useState([])
-  useEffect(() => {
-    llamadaApi()   
-  }, [dateYear, dateMouth, dateDay])
+}
 
   return (
       <div className="container">
@@ -63,19 +81,33 @@ const llamadaApi = async () => {
 
         <div className="container mb-2 rounded">
           <div className="row shadow-lg rounded">
+
+            {/* Restar Dia Botton */}
             <div className="col-4 d-flex justify-content-end">
                 <button className="btn btn-success" onClick={restarDia}> &lt; </button>
             </div>
-            <div className="col-4 d-flex justify-content-center">
-                Today
+
+            {/* DataPicker (Today) */}
+            <div className="col-4 d-flex justify-content-center flex-column">
+                <DatePicker id="datePicker"
+                   className="form-control text-center"
+                    selected={startDate} 
+                    onChange={date => setStartDate(date)}
+                    onCalendarClose={cambiarFecha}
+                    dateFormat="yyyy-MM-dd"
+                  />
+                
             </div>
+
+            {/* Sumar Dia Botton */}
             <div className="col-4 d-flex justify-content-start">
-                <button className="btn btn-success" onClick={sumarDia}> &gt; </button>
+                <button className="btn btn-success" onClick={sumarDia}>  &gt; </button>
             </div>
           </div>
         </div>
         
 
+        {/* Partidos del Dia */}
         {matches.map(match => 
           <div key={match.id}>
             <div className="container mb-3">
@@ -85,16 +117,18 @@ const llamadaApi = async () => {
                     <img className="rounded-circle" src={match.competition.area.ensignUrl} alt="" height="30px" width="30px" />
                 </div>
                 <div className="col-5 d-flex justify-content-end p-4 mb-2">
-                  <span>{match.homeTeam.name}</span>
+                  {/* <EquipoImage /> */}
+                  <span className="mx-2">{match.homeTeam.name}</span>
                 </div>
                 <div className="col-2 d-flex justify-content-center p-4 mb-2">
-                  <span>{match.score.fullTime.homeTeam != null ?
-                         <span>{match.homeTeam.name }</span> - <span>{match.homeTeam.name}</span> :
-                          match.utcDate.slice(11, -4) }
-                  </span>
+                  {match.score.fullTime.homeTeam != null ?
+                          <span> {match.score.fullTime.homeTeam} &#45; {match.score.fullTime.awayTeam} </span>:
+                          <span> {match.utcDate.slice(11, -4)} </span> }
+                  
                 </div>
                 <div className="col-5 d-flex justify-content-start p-4 mb-2">
-                  <span>{match.awayTeam.name}</span>
+                  <span className="mx-2">{match.awayTeam.name}</span>
+                  {/* <EquipoImage /> */}
                 </div>
 
               </div>
---
+++ src/Components/EquipoImage.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+
+class EquipoImage extends Component{
+ render(){
+    return(
+        
+            
+                    <img class="rounded-circle" src="https://images.fotmob.com/image_resources/logo/teamlogo/9906_xsmall.png" alt="" height="35px" width="35px"/>
+                
+        
+        
+    )
+ }    
+}
+
+export default EquipoImage
